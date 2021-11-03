3b74d6870b280235a10283908d0b243bf70184e6
s2100688 <89017210+s2100688@users.noreply.github.com>
2021-11-03 10:10:20 +0200

    added some CSS and img to animal single page

--- src/components/Animals/AnimalsSingle.js
+++ src/components/Animals/AnimalsSingle.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { useParams, useHistory } from "react-router-dom";
 
-const AnimalsSingle = () => {
+const AnimalsSingle = (props) => {
   let { animal } = useParams();
   let history = useHistory();
 
   return (
     <div className="animalsingle">
       <h1>This is the page for {animal} </h1>
+      <img
+        alt="Images from unsplash"
+        src={`https://source.unsplash.com/1600x900/?${animal}`}
+      />
       <div>
         <button onClick={() => history.goBack()}>Back to Animals</button>
       </div>
