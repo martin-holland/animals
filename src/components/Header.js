012332f15bcea32f84356af91a4e9633a76a979e
s2100688 <89017210+s2100688@users.noreply.github.com>
2021-11-09 14:40:47 +0200

    updated readme with screenshots

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,12 @@ import NavBar from "./NavBar";
 const Header = () => {
   return (
     <header>
+      <h1>Animals App</h1>
       <div className="header-wrapper">
         <img
           alt="logo"
           src="https://martin-holland.github.io/assets/LogoWhite.svg"
         />
-        <h1>Animals App</h1>
         <NavBar />
       </div>
     </header>
