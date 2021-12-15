884f71a8898ed06b115b48a057b056f3203adcae
s2100688 <89017210+s2100688@users.noreply.github.com>
2021-12-15 12:30:31 +0200

    added wikipedia info

--- src/components/Animals/AnimalsSingle.js
+++ src/components/Animals/AnimalsSingle.js
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams, useHistory } from "react-router-dom";
 
 const AnimalsSingle = (props) => {
   let { animal } = useParams();
   let history = useHistory();
+  const [contents, setContents] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
+
+  const endpoint = `https://en.wikipedia.org/w/api.php?action=query&origin=*&prop=extracts&format=json&exintro=true&titles=${animal}`;
+
+  const retrieveContents = (json) => {
+    const { pages } = json.query;
+    return Object.keys(pages).map((id) => pages[id].extract);
+  };
+
+  const getContents = async () => {
+    let resp;
+    let contents = [];
+    setLoading(true);
+    try {
+      resp = await fetch(endpoint);
+      const json = await resp.json();
+      contents = retrieveContents(json);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+    setContents(contents);
+  };
+
+  useEffect(() => {
+    getContents();
+  }, []);
+
+  if (loading) return "Please wait, Loading....";
+  if (error) return "Error occured while getting data.";
 
   return (
     <div className="animalsingle">
@@ -12,6 +45,15 @@ const AnimalsSingle = (props) => {
         alt="Images from unsplash"
         src={`https://source.unsplash.com/1600x900/?${animal}`}
       />
+      <div className="animalinfobox">
+        {contents.map((content) => (
+          <div
+            className="animalinfo"
+            dangerouslySetInnerHTML={{ __html: content }}
+          />
+        ))}
+      </div>
+
       <div>
         <button onClick={() => history.goBack()}>Back to Animals</button>
       </div>
